f989b5b0c6b65bcb61465525dd9508b150167c82
taxio <taxio.2029@gmail.com>
2019-11-26 15:01:43 +0900

    add app

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("argument error")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+
+	// check path exists
+	_, err := os.Stat(p)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("%s not exists.\n", p)
+		} else {
+			fmt.Println(err)
+		}
+		os.Exit(1)
+	}
+
+	// convert to abs path
+	absPath, err := filepath.Abs(p)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// print
+	fmt.Println(absPath)
+}
